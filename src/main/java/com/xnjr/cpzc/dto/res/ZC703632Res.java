f8cf3ebb725a6107e9639e6d6f2cdaea78f9ac13
pyez1158 <pyez1158@163.com>
2015-08-30 10:17:56 +0800

    Res缺少字段

--- src/main/java/com/xnjr/cpzc/dto/res/ZC703632Res.java
+++ src/main/java/com/xnjr/cpzc/dto/res/ZC703632Res.java
@@ -14,5 +14,70 @@ package com.xnjr.cpzc.dto.res;
  * @history:
  */
 public class ZC703632Res {
+    // 菜单编号
+    private String menuCode;
+
+    // 菜单名称
+    private String menuName;
+
+    // 菜单链接地址
+    private String menuUrl;
+
+    // 父菜单编号
+    private String parentCode;
+
+    // 菜单位置编号
+    private String orderNo;
+
+    // 备注
+    private String remark;
+
+    public String getMenuCode() {
+        return menuCode;
+    }
+
+    public String getMenuUrl() {
+        return menuUrl;
+    }
+
+    public void setMenuUrl(String menuUrl) {
+        this.menuUrl = menuUrl;
+    }
+
+    public String getOrderNo() {
+        return orderNo;
+    }
+
+    public void setOrderNo(String orderNo) {
+        this.orderNo = orderNo;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public void setMenuCode(String menuCode) {
+        this.menuCode = menuCode;
+    }
+
+    public String getParentCode() {
+        return parentCode;
+    }
+
+    public void setParentCode(String parentCode) {
+        this.parentCode = parentCode;
+    }
+
+    public String getMenuName() {
+        return menuName;
+    }
+
+    public void setMenuName(String menuName) {
+        this.menuName = menuName;
+    }
 
 }
